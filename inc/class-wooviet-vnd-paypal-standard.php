690312dd8733bc9561590e5e39e620a40914c819
Dat Hoang <htdat@users.noreply.github.com>
2018-05-12 07:28:54 +0000

    Revert this commit 1ad14ee9b898412432cd75dee7bc6b802d0ceb0a. Wrong approach

--- inc/class-wooviet-vnd-paypal-standard.php
+++ inc/class-wooviet-vnd-paypal-standard.php
@@ -43,9 +43,6 @@ class WooViet_VND_PayPal_Standard {
 
 		// Add the exchange rate info for this gateway in the checkout page before proceeding in the PayPal pages
 		add_filter( 'option_woocommerce_paypal_settings', array( $this, 'add_exchange_rate_info' ), 11 );
-
-		// Match currency of Paypal with local order
-		add_action( 'valid-paypal-standard-ipn-request', array( $this, 'match_currency_order' ), 10 );
 	}
 
 	/**
@@ -108,20 +105,6 @@ class WooViet_VND_PayPal_Standard {
 		}
 
 		return $value;
-	}
-
-	/*	
-	* Match response currency from Paypal IPN with the order
-	* 
-	* Topic https://wordpress.org/support/topic/loi-order-bi-on-hold/
-	*
-	* @author 	Longkt
-	* @since 	1.4
-	*/
-	public function match_currency_order($posted) {
-		if($posted['mc_currency']) {
-			$posted['mc_currency'] = $order->get_currency();
-		}		
 	}
 
 }
